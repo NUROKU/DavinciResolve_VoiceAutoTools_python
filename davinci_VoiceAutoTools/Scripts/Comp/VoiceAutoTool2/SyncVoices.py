18c8e34be5b74c6c4b176f04058e03eba854619d
NUROKU <shimau6@yahoo.co.jp>
2022-02-12 00:12:25 +0900

    区切り文字がWindowsに対応してないものだったので置換処理追加

--- davinci_VoiceAutoTools/Scripts/Comp/VoiceAutoTool2/SyncVoices.py
+++ davinci_VoiceAutoTools/Scripts/Comp/VoiceAutoTool2/SyncVoices.py
@@ -4,17 +4,22 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
 from tkinter import messagebox
+
 # 参照パスにModuleフォルダ追加
-sys.path.append(r"C:\ProgramData\Blackmagic Design\DaVinci Resolve\Fusion\Modules\VoiceAutoTools")
+sys.path.append(
+    r"C:\ProgramData\Blackmagic Design\DaVinci Resolve\Fusion\Modules\VoiceAutoTools"
+)
 
 from Usecase.SyncVoices2MediaPool import SyncVoices2MediaPool
 from Usecase.PullVoice2MediaPool import PullVoice2MediaPool
 from Config.VoiceAutoToolConfig import VoiceAutoToolConfig
 
+
 class CheckLog(tk.Checkbutton):
     """
     フォルダ監視ON/OFF切替チェックボタン
     """
+
     def __init__(self, root, resolve):
         self.var = tk.BooleanVar(root, value=False)
 
@@ -23,7 +28,7 @@ class CheckLog(tk.Checkbutton):
             text="←ここがONの場合、自動でSyncされます",
             variable=self.var,
             command=self.switch_logging,
-            )
+        )
         self.pack(anchor=tk.W)
 
         self.voice_syncer = SyncVoices2MediaPool(resolve)
@@ -34,47 +39,53 @@ class CheckLog(tk.Checkbutton):
         else:
             self.voice_syncer.Stop()
 
+
 class FolderPathSelectButton(tk.Button):
     """
     Sync元フォルダ選択ボタン
     """
+
     def __init__(self, root, resolve):
         super().__init__(
             root,
             text="参照",
             command=self.dirdialog_clicked,
-            )
+        )
         self.pack(anchor=tk.W)
 
         self.config = VoiceAutoToolConfig.get()
         self.IDirEntry = ttk.Entry(width=100)
-        self.IDirEntry.insert(tkinter.END,self.config["folder_path"])
+        self.IDirEntry.insert(tkinter.END, self.config["folder_path"])
         self.IDirEntry.pack(anchor=tk.W)
 
     def dirdialog_clicked(self):
         fld = filedialog.askdirectory(initialdir=dir)
-        self.IDirEntry.delete(0,tkinter.END)
-        self.IDirEntry.insert(tkinter.END,fld)
+        # 区切り文字がunix系のになってるのでここで置換
+        fld = fld.replace("/", "\\")
+        self.IDirEntry.delete(0, tkinter.END)
+        self.IDirEntry.insert(tkinter.END, fld)
         self.config["folder_path"] = fld
         VoiceAutoToolConfig.set(self.config)
+
 
 class PullButton(tk.Button):
     """
     一括Pullボタン
     """
+
     def __init__(self, root, resolve):
         self.resolve = resolve
         super().__init__(
             root,
             text="全てをPull",
             command=self.pullbutton_clicked,
-            )
+        )
         self.pack(anchor=tk.W)
 
     def pullbutton_clicked(self):
         pullVoice2MediaPool = PullVoice2MediaPool()
         pullVoice2MediaPool.Execute(self.resolve)
-        
+
         messagebox.showinfo("SyncVoice", "ボイスのPullが完了しました。")
 
 
@@ -85,7 +96,7 @@ root = tk.Tk()
 root.title("VoiceAutoTool_VoiceSyncer")
 root.geometry("400x200")
 
-label1 = tk.Label(text='Sync元のフォルダパス設定')
+label1 = tk.Label(text="Sync元のフォルダパス設定")
 label1.pack(anchor=tk.W)
 
 b1 = FolderPathSelectButton(root, resolve)
